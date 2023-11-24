2ab06ec38a625eb84f2fa173afe2a1f067b322f9
everlonery <everlonery@gmail.com>
2023-11-24 09:59:45 +0200

    Database project deleted

--- StoresNetwork/Database/DatabaseConnection.cs
+++ StoresNetwork/Database/DatabaseConnection.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace StoresNetwork.Database
 {
-    internal class DatabaseConnection
+    internal static class DatabaseConnection
     {
         private const string ConnectionString = "Data Source=WS-000030129;Initial Catalog=BikeStores;Persist Security Info=False;User ID=sa;Password=12345678";
+        public static SqlConnection GetConnection() => new(ConnectionString);
+
+        #region Cities
+
+        #endregion
     }
 }
